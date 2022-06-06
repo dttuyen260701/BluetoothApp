f0d74ba0dce5f2b84d24f6f2111e65d65c621e94
Tuyen <dttuyen260701@gmail.com>
2022-06-06 16:59:11 +0700

    Tuyen commit: Continue

--- app/src/main/java/com/example/bluetoothapp/fragments/Fragment_Control.java
+++ app/src/main/java/com/example/bluetoothapp/fragments/Fragment_Control.java
@@ -1,5 +1,6 @@
 package com.example.bluetoothapp.fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import androidx.fragment.app.Fragment;
 
@@ -17,11 +19,12 @@ import com.example.bluetoothapp.Utils.Constant_Values;
 
 public class Fragment_Control extends Fragment {
     private TextView txtPower_Control_Frag;
-    private ImageButton btn_Compartment, btn_Inside_door,
-            btnPower_Ctrl_Frag;
-    private ImageView img_Descrip_Control_Frag;
+    private ImageButton btn_down_left, btn_down_right,
+            btnMode_control_Frag;
+    private ImageView img_Descrip_Control_Frag, btn_servor;
+    private VideoView video_Preview_Control;
     private Garbage_Can garbage_can;
-    private long mLastClick_Power = 0, mLastClick_Close = 0,
+    private long mLastClick_Mode = 0, mLastClick_Sweep = 0,
             mLastClick_LRDoor = 0;
 
     @Override
@@ -35,10 +38,12 @@ public class Fragment_Control extends Fragment {
     }
 
     private void SetUp(View view){
-        btn_Compartment = (ImageButton) view.findViewById(R.id.btn_down_left);
-        btn_Inside_door = (ImageButton) view.findViewById(R.id.btn_down_right);
-        btnPower_Ctrl_Frag = (ImageButton) view.findViewById(R.id.btnPower_Ctrl_Frag);
+        btn_down_left = (ImageButton) view.findViewById(R.id.btn_down_left);
+        btn_down_right = (ImageButton) view.findViewById(R.id.btn_down_right);
+        btnMode_control_Frag = (ImageButton) view.findViewById(R.id.btnMode_control_Frag);
         img_Descrip_Control_Frag = (ImageView) view.findViewById(R.id.img_Descrip_Control_Frag);
+        btn_servor = (ImageView) view.findViewById(R.id.btn_servor);
+        video_Preview_Control = (VideoView) view.findViewById(R.id.video_Preview_Control);
         updateView();
 
         if(garbage_can.isMode()){
@@ -57,51 +62,71 @@ public class Fragment_Control extends Fragment {
             }
         });
 
-        btn_Compartment.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (SystemClock.elapsedRealtime() - mLastClick_Close < 4000){
-                    return;
-                }
-
-            }
-        });
-
-        btn_Inside_door.setOnClickListener(new View.OnClickListener() {
+        btn_down_left.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (SystemClock.elapsedRealtime() - mLastClick_LRDoor < 4000){
                     return;
                 }
                 mLastClick_LRDoor = SystemClock.elapsedRealtime();
-
+                String path = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.tournado_rev;
+                video_Preview_Control.setVideoURI(Uri.parse(path));
+                video_Preview_Control.start();
             }
         });
 
-        btnPower_Ctrl_Frag.setOnClickListener(new View.OnClickListener() {
+        btn_down_right.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (SystemClock.elapsedRealtime() - mLastClick_Power < 4000){
+                if (SystemClock.elapsedRealtime() - mLastClick_LRDoor < 4000){
                     return;
                 }
-                mLastClick_Power = SystemClock.elapsedRealtime();
+                mLastClick_LRDoor = SystemClock.elapsedRealtime();
+                String path = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.wind_mill;
+                video_Preview_Control.setVideoURI(Uri.parse(path));
+                video_Preview_Control.start();
+            }
+        });
 
+        btn_servor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (SystemClock.elapsedRealtime() - mLastClick_Sweep < 4000){
+                    return;
+                }
+                mLastClick_Sweep = SystemClock.elapsedRealtime();
+                String path = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.vertical_rect;
+                video_Preview_Control.setVideoURI(Uri.parse(path));
+                video_Preview_Control.start();
+            }
+        });
+
+        btnMode_control_Frag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (SystemClock.elapsedRealtime() - mLastClick_Mode < 4000){
+                    return;
+                }
+                mLastClick_Mode = SystemClock.elapsedRealtime();
+                String path = "android.resource://" + getActivity().getPackageName() + "/" + R.raw.vertical_rect;
+                video_Preview_Control.setVideoURI(Uri.parse(path));
+                video_Preview_Control.start();
             }
         });
     }
 
     private void isControl(){
         //dong cua k nhan rac
-        btnPower_Ctrl_Frag.setImageResource(R.drawable.power_on);
-        btn_Compartment.setEnabled(false);
-        btn_Inside_door.setEnabled(false);
+        btnMode_control_Frag.setImageResource(R.drawable.power_on);
+        btn_down_left.setEnabled(false);
+        btn_down_right.setEnabled(false);
     }
 
     private void isAuto(){
         //mo cua thung rac
-        btnPower_Ctrl_Frag.setImageResource(R.drawable.power_off);
-        btn_Compartment.setEnabled(true);
-        btn_Inside_door.setEnabled(true);
+        btnMode_control_Frag.setImageResource(R.drawable.power_off);
+        btn_down_left.setEnabled(true);
+        btn_down_right.setEnabled(true);
     }
 
 
